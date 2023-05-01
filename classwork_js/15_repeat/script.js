50b506c13357df31e6749b4fab724e76c574be77
Aliyeva Zarifa <126771990+AliyevaZarifa8@users.noreply.github.com>
2023-05-01 08:09:58 -0700

    task compiliated

---
+++ classwork_js/15_repeat/script.js
@@ -0,0 +1,179 @@
+const arr=['6',8,NaN,'0',5,4,'true',false,7,'a',undefined,8,true]
+///////////////////////////1
+// const falsyValue=(arrey)=>{
+// let newArr=[]
+// for (let i = 0; i < arrey.length; i++) {
+//   if(!arrey[i]){
+
+//     newArr.push(arrey[i])
+//   }
+    
+// }return newArr
+// } 
+// console.log(falsyValue(arr));
+
+// ////////////////////2
+
+// const stringValue=(arrey)=>{
+//     let newArr=[]
+//     for (let i = 0; i < arrey.length; i++) {
+//       if(typeof(arrey[i])==="string" ){
+    
+//         newArr.push(arrey[i])
+//       }
+        
+//     }return newArr
+//     } 
+//     console.log(stringValue(arr));
+
+//     ///////////////////3
+//     const sliceArr=(arrey)=>{
+//         let newArr=[]
+//         for (let i = 3; i <= 7; i++) {
+//           if(true){
+        
+//             newArr.push(arrey[i])
+//           }
+            
+//         }return newArr
+//         } 
+//         console.log(sliceArr(arr));
+
+// ////////////4
+// const sumArr=(arrey)=>{
+//     let sum=0
+//     for (let i = 0; i < arrey.length; i++) {
+//       if(typeof(arrey[i])==="number" && arrey[i] ){
+    
+//        sum+=arrey[i]
+//       }
+        
+//     }return sum
+//     } 
+//     console.log(sumArr(arr));
+//     /////////////////////5
+
+
+//     const findlastArr=(arrey)=>{
+//        let count=0
+//         for (let i = 2; i < arrey.length; i++) {
+//           if(arrey[i]===8 ){
+        
+//            count+=i
+//           }
+            
+//         }return count
+//         } 
+//         console.log(findlastArr(arr));
+
+
+
+
+//////////////////6
+
+    // const reverseArr=(array)=>{
+    //     let newArr=[]
+    //     for (let i = array.length-1; i > 0; i--) {
+    //       if(true){
+
+    //       newArr.push(array[i])
+    //       }
+            
+    //     }return newArr
+    //     } 
+    //     console.log(reverseArr(arr));
+
+
+    /////////////////////7
+    const arrNum=[3,1,22,0,12,11,9,8,10,14,41,55,51]
+   
+    //     const sortarrNum=(arrey)=>{
+    //     for (let i = 0; i <arrey.length; i++) {
+    //     for (let j = 0; j<i; j++){
+    //     if(arrey[i]<arrey[j]){
+    //        let  x=arrey[i]
+    //        arrey[i]=arrey[j]
+    //        arrey[j]=x
+    //     }
+    //         }
+    //     }return arrey
+    //     } 
+    //     console.log(sortarrNum(arrNum));
+
+
+    // ////////////////8
+    const arrLetters=['g','d','a','c','j','b','e']
+
+    // const sortArr=(arrey)=>{
+    //     for (let i = 0; i <arrey.length; i++) {
+    //         for (let j = 0; j<i; j++){
+    //         if(arrey[i]<arrey[j]){
+    //            let  x=arrey[i]
+    //            arrey[i]=arrey[j]
+    //            arrey[j]=x
+    //         }
+    //             }
+    //         }return arrey
+    // }
+    // console.log(sortArr(arrLetters));
+
+    //////////////////9
+    const arr9=[[1,2],[3,4,5],[6,7,8,9]]
+
+    // const flatArr=(array)=>{
+    //  let newArr=[]
+    //  for (let i = 0; i <array.length; i++) {
+    //     for (let j = 0; j<array[i].length; j++){
+
+    //   newArr.push(array[i][j])
+    // }
+    //   }return newArr
+    // }
+    //  console.log(flatArr(arr9));
+
+
+    // let calculator={
+
+    //    sum(a,b){
+    //    return this.a+this.b
+    //    },
+    //    mul(a,b){
+    //    return this.a*this.b
+    //    },
+    //    div(a,b){
+    //    return this.a/this.b
+    //    },
+       
+    // }
+    // console.log(calculator.sum(4,5));
+
+
+
+////////////////////Calculiator
+
+// let operator;
+// let number1 ;
+// let number2 ;
+// let result;
+
+// if (operator == '+') {
+//     result = number1 + number2;
+// }
+// else if (operator == '-') {
+//     result = number1 - number2;
+// }
+// else if (operator == '*') {
+//     result = number1 * number2;
+// }
+// else {
+//     result = number1 / number2;
+// }
+
+// console.log(8+7);
+// console.log(8-7);
+// console.log(8*7);
+// console.log(Math.ceil(8/7));
+
+
+
+///////////////SECOND WAY
