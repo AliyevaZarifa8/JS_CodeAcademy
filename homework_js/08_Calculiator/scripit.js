aff0d9ae32f16a6345d0c64116ef4b2f99bb2f51
AliyevaZarifa8 <aliyevazarifa061@gmail.com>
2023-05-02 11:47:41 -0700

    task uploaded

--- homework_js/08_Calculiator/scripit.js
+++ homework_js/08_Calculiator/scripit.js
@@ -1,4 +1,4 @@
-let inputSum = document.querySelector("#valuesum");
+let paragSum = document.querySelector("#valuesum");
 let inputValue1 = document.querySelector("#value1");
 let inputValue2 = document.querySelector("#value2");
 let additionBtn = document.querySelector("#addition");
@@ -7,30 +7,30 @@ let multiplyBtn = document.querySelector("#multiply");
 let divisionBtn = document.querySelector("#division");
 let resetBtn = document.querySelector("#reset");
 
-inputSum.value = 0;
+paragSum.innerText = 0;
 
 additionBtn.addEventListener("click", function () {
     inputValue1.value==="" || inputValue2.value===""
     ? alert("Pls enter a number in value1 vs value2 input")
-    :inputSum.value = +inputValue1.value + +inputValue2.value;
+    :paragSum.innerText = +inputValue1.value + +inputValue2.value;
 });
 substractBtn.addEventListener("click", function () {
     inputValue1.value==="" || inputValue2.value===""
     ? alert("Pls enter a number in value1 vs value2 input")
-    :inputSum.value = +inputValue1.value - +inputValue2.value;
+    :paragSum.innerText = +inputValue1.value - +inputValue2.value;
 });
 multiplyBtn.addEventListener("click", function () {
     inputValue1.value==="" || inputValue2.value===""
     ? alert("Pls enter a number in value1 vs value2 input")
-    :inputSum.value = +inputValue1.value * +inputValue2.value;
+    :paragSum.innerText = +inputValue1.value * +inputValue2.value;
 });
 divisionBtn.addEventListener("click", function () {
     inputValue1.value==="" || inputValue2.value==="" || inputValue2.value==0
     ? alert("Pls enter the correct numbers for the calculator to work")
-    :inputSum.value = Math.round(+inputValue1.value / +inputValue2.value)
+    :paragSum.innerText = (+inputValue1.value / +inputValue2.value).toFixed(2)
 });
 resetBtn.addEventListener("click", function () {
     inputValue1.value==="" || inputValue2.value===""
     ? alert("Pls enter a number in value1 vs value2 input")
-    :inputSum.value = 0 ,inputValue1.value="" , inputValue2.value="";
+    :paragSum.innerText = 0 ,inputValue1.value="" , inputValue2.value="";
 });
